b9e2e00adb374aa19748a9b25d4cc26e1db435e7
kubilayagi <kubilay.agi@gmail.com>
2018-04-14 14:52:59 -0700

    html stuff

---
+++ p1b/query.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>1B</title>
+</head>
+<body>
+	<form action="." method="GET">
+		<textarea name="query" cols="60" rows="8"></textarea>
+		<input type="submit" name="">
+	</form>
+
+</body>
+</html>
