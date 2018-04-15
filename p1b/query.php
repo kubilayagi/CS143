a756982fedcfbe207fe1f095cc08e07dd4b4b17a
kubilayagi <kubilay.agi@gmail.com>
2018-04-14 18:04:41 -0700

    added some php query stuff

--- p1b/query.php
+++ p1b/query.php
@@ -8,5 +8,19 @@
 		<input type="submit" name="submit">
 	</form>
 
+	<?php
+		$db = new mysqli('localhost', 'cs143', '', 'TEST');
+
+		if($db->connect_errno > 0){
+		    die('Unable to connect to database [' . $db->connect_error . ']');
+		}
+
+		if(isset($_GET['query'])) {
+			$query=$_GET["query"];
+
+		$result = $db->query($query);
+		echo $result;
+	?>
+
 </body>
 </html>
