7e88bd1b1d2918b95b66fdfe395155f27639f742
Lawrence Chen <lawrencechen98@gmail.com>
2018-04-14 23:07:56 -0700

    Added all integrity constraints, checks, and comments, including in the readme file

--- p1b/create.sql
+++ p1b/create.sql
@@ -1,54 +1,52 @@
 create table Movie (
-	id 		int,
-	title 	varchar(100),
-	year 	int,
+	id 		int not null,
+	title 	varchar(100) not null,
+	year 	int not null,
 	rating 	varchar(10),
 	company varchar(50),
-	primary key(id),
-	check(id<=4750) #id must be equal to the max id number + 1 for 1c, but for now, less than 4750
+	primary key(id), #id is the unique identifier for Movie table
+	check(year > 1800), #movie release year should definitely be at least after year 1800, considering that movies weren't invented till the late 19th century
+	check(rating='G' OR rating='PG' OR rating='PG-13' OR rating='R' OR rating='NC-17' OR rating='NR' OR rating='UR' or rating is null) #the MPAA rating must be one of the standard possible ratings that MPAA gives out to movies
 );
 
 create table Actor (
-	id 		int,
+	id 		int not null,
 	last 	varchar(20),
 	first 	varchar(20),
 	sex 	varchar(6),
-	dob 	date,
+	dob 	date not null,
 	dod 	date,
 	primary key(id), #id is the unique identifier for Actor table
-	check(id<=69000) #id must be equal to the max id number + 1 for 1c, but for now, less than 69000
+	check(dod is null or dod > dob) #either the Actor is not dead yet, or the date of death must be after they were born at the very least
 );
 
 create table Director (
-	id 		int,
+	id 		int not null,
 	last 	varchar(20),
 	first 	varchar(20),
-	dob 	date,
+	dob 	date not null,
 	dod 	date,
 	primary key(id), #id is the unique identifier for Director table
-	check(id<=69000) #id must be equal to the max id number + 1 for 1c, but for now, less than 69000
+	check(dod is null or dod > dob) #either the Director is not dead yet, or the date of death must be after they were born at the very least
 );
 
 create table MovieGenre (
-	mid 	int,
+	mid 	int not null,
 	genre 	varchar(20),
-	primary key(mid) #id is the unique identifier for Movie table
 	foreign key (mid) references Movie(id) #movie id is primary key for Movie table
 ) ENGINE=INNODB;
 
 create table MovieDirector (
-	mid int,
-	did int,
-	primary key(mid, did) #mid and did combination is the unique identifier for MovieDirector table
+	mid int not null,
+	did int not null,
 	foreign key (mid) references Movie(id), #movie id is primary key for Movie table
 	foreign key (did) references Director(id) #director id is primary key for Director table
 ) ENGINE=INNODB;
 
 create table MovieActor (
-	mid 	int,
-	aid 	int,
+	mid 	int not null,
+	aid 	int not null,
 	role 	varchar(50),
-	primary key(mid, aid), #mid and aid combination is the unique identifier for MovieActor table
 	foreign key (mid) references Movie(id), #movie id is primary key for Movie table
 	foreign key (aid) references Actor(id) #actor id is primary key for Actor table
 ) ENGINE=INNODB;
@@ -56,11 +54,11 @@ create table MovieActor (
 create table Review (
 	name 	varchar(20),
 	time 	timestamp,
-	mid 	int,
-	rating 	int,
+	mid 	int not null,
+	rating 	int not null,
 	comment varchar(500),
-	primary key(name, time), #name and time combination is the unique identifier for Review table, assuming no review can be posted about the same movie at the same time
-	foreign key (mid) references Movie(id) #movie id is primary key for Movie table
+	foreign key (mid) references Movie(id), #movie id is primary key for Movie table
+	check(rating >= 1 AND rating <= 5) #ratings are only out of 5, so the only possible scores are 1 through 5
 ) ENGINE=INNODB;
 
 create table MaxPersonID (
--- p1b/readme.txt
+++ p1b/readme.txt
@@ -1,5 +1,26 @@
 # Project 1B
 
+Enforcing Data Integrity
+Primary Key Restraints:
+	Actor IDs
+	Movie IDs
+	Director IDs
+Referential Integrity Restraints
+	MovieGenre's mid is a foreign key referencing Movie's id
+	Review's mid is a foreign key referencing Movie's id
+	MovieDirector's mid is a foreign key referencing Movie's id
+	MovieActor's mid is a foreign key referencing Movie's id
+	MovieActor's aid is a foreign key referencing Actor's id
+	MovieDirector's did is a foreign key referencing Director's id
+Check Constraints
+	Movie release year should not be before 1800s, considering movies were invented in late 19th century
+	Movie MPAA rating should be either null or one of the possible categories, 'G','PG','PG-13','R','NC-17','NR','UR'
+	Director's date of death should either be null or after the date of birth
+	Review rating is out of 5 (according to specs like Amazon's review system) so should be 1 through 5
+
+
+
+
 For this project, we used several resources including Stack Overflow and W3Schools.
 Many of the searches that were done were syntactic questions. The following links
 helped us understand which function calls were appropriate for our desired
