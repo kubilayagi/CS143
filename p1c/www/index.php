00e1c41a43e093fe75ffd64c556d16edd231c007
Lawrence Chen <lawrencechen98@gmail.com>
2018-04-28 23:46:16 -0700

    Added some descriptions on our index page

--- p1c/www/index.php
+++ p1c/www/index.php
@@ -13,5 +13,18 @@
 	<a href="./addcomments.php">Add Comments</a>
 	<a href="./add_actor_to_movie.php">Add Actor to Movie Relation</a>
 	<a href="./add_director_to_movie.php">Add Director to Movie Relation</a>
+
+	<br>
+	<h1>Welcome to Our Knockoff IMDB!</h1>
+	<h3>Search</h3>
+	<p>Use this page to search up any of your favorite actors or movies! Just type in keywords of the actor's name or movie title, and related results will be outputted. Click on a result to get more info about the actor or movie.</p>
+	<h3>Add Person</h3>
+	<p>You can also add new people into our database. Click this page to add a new director or actor, along with their details, such as birthdate, gender, etc.</p>
+	<h3>Add Movie</h3>
+	<p>Don't see a movie you're looking for? Help us out by adding it to our database. You can add a new movie entry, along with its release data, MPAA rating, and genre.</p>
+	<h3>Assign Actor to Movie</h3>
+	<p>Did we forget an actor's role in a movie? Use this page to indicate an actor's appearance and the character they appeared as.</p>
+	<h3>Assign Director to Movie</h3>
+	<p>Finally, movies also needs directors. If you see that a movie does not have a director listed, you can help us indicate the projects a specific director has worked on here.</p>
 </body>
 </html>
