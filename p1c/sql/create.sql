f93bc2f8bc5d18b94749c7be1df6c282ac39d0ff
Lawrence Chen <lawrencechen98@gmail.com>
2018-04-30 18:56:46 -0700

    New sql script

--- p1c/sql/create.sql
+++ p1c/sql/create.sql
@@ -1,3 +1,13 @@
+drop table if exists MovieGenre;
+drop table if exists MovieDirector;
+drop table if exists MovieActor;
+drop table if exists Review;
+drop table if exists Movie;
+drop table if exists Actor;
+drop table if exists Director;
+drop table if exists MaxPersonID;
+drop table if exists MaxMovieID;
+
 create table Movie (
 	id 		int not null,
 	title 	varchar(100) not null,
