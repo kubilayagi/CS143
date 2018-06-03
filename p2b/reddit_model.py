ce2acd4d8aa73d48acbe00f0b2475e42a6bfbd1a
Lawrence Chen <lawrencechen98@gmail.com>
2018-06-03 00:08:27 -0700

    Trained models

--- p2b/reddit_model.py
+++ p2b/reddit_model.py
@@ -81,14 +81,59 @@ def main(context):
     
     
     
-    #TASK 6B
-    # Code for task 6B
+    # TASK 6B
+    # Code for task 6B...
 	vectorized.createOrReplaceTempView("vectorized")
-	labeled = context.sql("select *, case when labeldjt = 1 then 1 else 0 end as posLabel, case when labeldjt = -1 then 1 else 0 end as negLabel from vectorized")
+	labeled = context.sql("select *, case when labeldjt = 1 then 1 else 0 end as poslabel, case when labeldjt = -1 then 1 else 0 end as neglabel from vectorized")
 	labeled.show()
 
+	# TASK 7
+	# Code for task 7...
+	pos = labeled
+	neg = labeled
 
+	# Bunch of imports (may need more)
+	from pyspark.ml.classification import LogisticRegression
+	from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
+	from pyspark.ml.evaluation import BinaryClassificationEvaluator
+	# Initialize two logistic regression models.
+	# Replace labelCol with the column containing the label, and featuresCol with the column containing the features.
+	poslr = LogisticRegression(labelCol="poslabel", featuresCol="features", maxIter=10)
+	neglr = LogisticRegression(labelCol="neglabel", featuresCol="features", maxIter=10)
+	# This is a binary classifier so we need an evaluator that knows how to deal with binary classifiers.
+	posEvaluator = BinaryClassificationEvaluator()
+	negEvaluator = BinaryClassificationEvaluator()
+	# There are a few parameters associated with logistic regression. We do not know what they are a priori.
+	# We do a grid search to find the best parameters. We can replace [1.0] with a list of values to try.
+	# We will assume the parameter is 1.0. Grid search takes forever.
+	posParamGrid = ParamGridBuilder().addGrid(poslr.regParam, [1.0]).build()
+	negParamGrid = ParamGridBuilder().addGrid(neglr.regParam, [1.0]).build()
+	# We initialize a 5 fold cross-validation pipeline.
+	posCrossval = CrossValidator(
+	    estimator=poslr,
+	    evaluator=posEvaluator,
+	    estimatorParamMaps=posParamGrid,
+	    numFolds=5)
+	negCrossval = CrossValidator(
+	    estimator=neglr,
+	    evaluator=negEvaluator,
+	    estimatorParamMaps=negParamGrid,
+	    numFolds=5)
+	# Although crossvalidation creates its own train/test sets for
+	# tuning, we still need a labeled test set, because it is not
+	# accessible from the crossvalidator (argh!)
+	# Split the data 50/50
+	posTrain, posTest = pos.randomSplit([0.5, 0.5])
+	negTrain, negTest = neg.randomSplit([0.5, 0.5])
+	# Train the models
+	print("Training positive classifier...")
+	posModel = posCrossval.fit(posTrain)
+	print("Training negative classifier...")
+	negModel = negCrossval.fit(negTrain)
 
+	# Once we train the models, we don't want to do it again. We can save the models and load them again later.
+	posModel.save("www/pos.model")
+	negModel.save("www/neg.model")
 
 
 
