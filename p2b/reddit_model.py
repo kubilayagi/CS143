ca33531c61bb78bdd63ce0a71aadd4f505d5d103
kubilayagi <kubilay.agi@gmail.com>
2018-06-02 17:35:54 -0700

    parquet working

--- p2b/reddit_model.py
+++ p2b/reddit_model.py
@@ -18,30 +18,32 @@ def main(context):
     comments_parquet = os.path.abspath("./comments-minimal.parquet")
     submissions_parquet = os.path.abspath("./submissions.parquet")
     labeled_data_parquet = os.path.abspath("./labeled_data.parquet")
+
+    if(os.path.exists(labeled_data_parquet)):
+        labeled_data_DF = sqlContext.read.parquet(labeled_data_parquet)
+    else:
+        labeled_data_DF = sqlContext.read.csv("labeled_data.csv")
+        labeled_data_DF.write.parquet(labeled_data_parquet)
+
+    if(os.path.exists(submissions_parquet)):
+        submissions_DF = sqlContext.read.parquet(submissions_parquet)
+    else:
+        submissions_DF = sqlContext.read.json("submissions.json.bz2")
+        submissions_DF.write.parquet(submissions_parquet)
+
     
     if(os.path.exists(comments_parquet)):
     	comments_DF = sqlContext.read.parquet(comments_parquet)
     else:
     	comments_DF = sqlContext.read.json("comments-minimal.json.bz2")
     	comments_DF.write.parquet(comments_parquet)
-
-    if(os.path.exists(submissions_parquet)):
-    	submissions_DF = sqlContext.read.parquet(submissions_parquet)
-    else:
-    	submissions_DF = sqlContext.read.json("submissions.json.bz2")
-    	submissions_DF.write.parquet(submissions_parquet)
-
-    if(os.path.exists(labeled_data_parquet)):
-    	labeled_data_DF = sqlContext.read.parquet(labeled_data_parquet)
-    else:
-    	labeled_data_DF = sqlContext.read.csv("labeled_data.csv")
-    	labeled_data_DF.write.parquet(labeled_data_parquet)
+    
 
     # TASK 2
 	# Code for task 2...
-	labeled_data_DF.createOrReplaceTempView("labeled_data")
-	comments_DF.createOrReplaceTempView("comments")
-	labeled_comments = context.sql("select * from labeled_data inner join comments on comment_id")
+    labeled_data_DF.createOrReplaceTempView("labeled_data")
+    comments_DF.createOrReplaceTempView("comments")
+    labeled_comments = context.sql("select * from labeled_data inner join comments on comment_id")
 
 if __name__ == "__main__":
     conf = SparkConf().setAppName("CS143 Project 2B")
